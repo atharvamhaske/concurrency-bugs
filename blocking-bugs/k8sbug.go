b5d24277c3a406739955afaa819a393a363b04be
atharvamhaske <atharvamhaske76@gmail.com>
2026-01-15 23:17:37 +0530

    wrote all learnings

--- blocking-bugs/k8sbug.go
+++ blocking-bugs/k8sbug.go
@@ -27,3 +27,36 @@ func doWork() string {
 	time.Sleep(2 * time.Second)
 	return "Working bhaiii"
 }
+
+
+//so the fix for this will be use buffered channels instead of unbuffered channels, so earlier when we have put same situation of both timer with unbuf
+//channel we clearly saw that we always counter with second case as it prints always returning cuz unbuffered channel has no size its buffer size is zero so its send 
+//operation is only valid when someone is recieving from it just like hand in hand handshake in daily life.
+// 
+//->But but its different in case of buffered channels it has fixed buffer size as we alot it while creating using make keyword so whatever happens like if theirs no recievr
+//or not we always can send on ch and its non blocking ops and due to it all three lines are printed here so to actually feel this pls run below code after doing with above
+//code
+
+
+func main() {
+	ch := make(chan any, 1)
+
+	go func() {
+		res := doWork()
+		fmt.Println("Trying to send work details on channels")
+		ch <- res                                //this blocks
+		fmt.Println("Details sent successfully") //will never reach this
+	}()
+	select {
+	case result := <-ch:
+		fmt.Println("main:got results:", result)
+
+	case <-time.After(2 * time.Second):
+		fmt.Println("main:timeout, returning")
+	}
+}
+
+func doWork() string {
+	time.Sleep(2 * time.Second)
+	return "Working bhaiii"
+}
