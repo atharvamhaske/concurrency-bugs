830bb2620ef38b608ae3644017118630d359723d
atharvamhaske <atharvamhaske76@gmail.com>
2026-02-02 23:39:56 +0530

    Implement non-blocking goroutines with WaitGroup to print version strings concurrently

--- non-blocking-bugs/main.go
+++ non-blocking-bugs/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	
+	wg := sync.WaitGroup{}
+
+	for i := 17; i <= 21; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			someRandom := fmt.Sprintf("v1.%d", i)
+			fmt.Println(someRandom)
+		}()
+	}
+
+	wg.Wait()
 }
